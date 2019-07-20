78ef4c9d0a16ed0f5fe3184ef7df708eb603f685
Mohamed Hassan Anwer <47162601+TheArtfulBro@users.noreply.github.com>
2019-07-21 01:14:13 +0200

    Update lord.py

--- lord.py
+++ lord.py
@@ -91,7 +91,12 @@ print('\n')
 ban('   lord')
 idt=input('[+]Enter your username : ')
 passw=input('[+]Enter your password : ')
-key=input('[+]Enter your key : ')
+while True:
+    key=input('[+]Enter your key : ')
+    if len(key) == 32:
+        break
+    else :
+        key=''
 key1=key
 url = ("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + (idt) + "&locale=en_US&password=" + (passw) + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 data= requests.get(url)
